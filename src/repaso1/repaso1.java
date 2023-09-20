f18e35619348179bccc027cde4ea019d91ed57d5
Miguel_Dev <miguelsg01@protonmail.com>
2023-09-20 19:35:09 +0200

    Initial commit about refresh Java

---
+++ src/repaso1/repaso1.java
@@ -0,0 +1,31 @@
+package repaso1;
+
+public class repaso1 {
+	public static void main(String[] args) {
+//		 int x = 10;
+//		 System.out.println(x);
+//		 x++;
+//		 System.out.println(x);
+//		 System.out.println(++x);
+//		 System.out.println(x++);
+//		 System.out.println(x);
+//		 System.out.println(x++);
+//		 System.out.println(++x);
+		 char A = 'c';
+		 char B;
+		 System.out.println(A++);
+		 System.out.println(A++);
+		 System.out.println(++A);
+		 System.out.println(A);
+		 B = --A;
+		 System.out.println(++A);
+		 A++;
+		 --B;
+		 System.out.println(B++);
+		 System.out.println(++B);
+		 System.out.println(++A);
+		 System.out.println(B--);
+		 System.out.println(A);
+		 System.out.println(B);
+	}
+}
---
+++ src/repaso2/ejercicio1.java
@@ -0,0 +1,20 @@
+package repaso2;
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+	public static void main(String[] args) {
+//		Calcula el factorial de un número pedido por teclado
+		System.out.print("Introduce numero para factorial: ");
+		Scanner keyboard = new Scanner(System.in);
+        int numero = keyboard.nextInt();
+        int result = 1;
+		for (int i = 1; i <= numero; i++) {
+			result = result * i;
+			System.out.println(result);
+		}
+		System.out.println("Final: " + result);
+		
+		
+	}
+}
